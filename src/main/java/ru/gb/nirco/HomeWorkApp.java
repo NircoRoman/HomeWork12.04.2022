5c0576d3b8a062e9326c49a4be3665b26f99ede7
amor2 <nircoroman@gmail.com>
2022-04-13 00:55:28 +0300

    HomeWork13.04.2022

---
+++ src/main/java/ru/gb/nirco/HomeWorkApp.java
@@ -0,0 +1,35 @@
+package ru.gb.nirco;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+    printThreeWords();
+    checkSumSing();
+    printColor();
+    compareNumbers();
+
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Aplle");
+    }
+    public static void checkSumSing(){
+        int a = 2;
+        int b =-3;
+        if (a >=0) System.out.println("Сумма положилельня a=" +a);
+        if (b <=-1) System.out.println("Сумма отрицательная b=" +b);
+    }
+    public static void printColor() {
+        int value = 0;
+        if (value<=0) System.out.println("Красный ");
+        if (value <=100 & value !=0) System.out.println("Желтый");
+        if (value >100) System.out.println("Зеленый");
+    }
+    public static void compareNumbers(){
+        int a,b;
+        a = 4;
+        b = 7;
+        if (a>=b) System.out.println("a>=b");
+        else System.out.println("a<b");
+    }
+}
