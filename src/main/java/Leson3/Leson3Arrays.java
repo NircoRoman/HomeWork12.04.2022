dd94ffe2472d592efe69606dfa670482011d623b
NircoRoman <nircoroman@gmail.com>
2022-04-19 22:13:33 +0300

    HomeWork19.04.2022

--- src/main/java/Leson3/Leson3Arrays.java
+++ src/main/java/Leson3/Leson3Arrays.java
@@ -1,14 +1,5 @@
 package Leson3;
-import java.util.Random;
-public class Leson3Arrays {
-    public static void Arrays (String args[]){
-            int[] arrays = new int[10];
-            int randomArray = new Random().nextInt(2);
-            for (int i = 0; i<=arrays.length; i++){
-                arrays[i] = randomArray;
-                System.out.print(arrays[i] + " ");
 
-        }
-    }
+public class Leson3Arrays {
 
 }
